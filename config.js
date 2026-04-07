c84e96dbda7cd152bed769679e03be84cddffcc7
reza021mohammadi <59869788+reza021mohammadi@users.noreply.github.com>
2026-04-07 12:33:38 +0200

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDNJdUV63copftWu0MdwUgFFGJSRV-sjuY",
+  authDomain:        "budget-tracker-4c83c.firebaseapp.com",
+  projectId:         "budget-tracker-4c83c",
+  storageBucket:     "budget-tracker-4c83c.firebasestorage.app",
+  messagingSenderId: "130617560083",
+  appId:             "1:130617560083:web:369e10ed924b2671c043e6",
+};
